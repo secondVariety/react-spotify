d6594ec9d20b423d907fcae0854d8c7f62e9cfd9
secondVariety <garydavison21@gmail.com>
2021-10-20 23:13:09 +0100

    Removing demo files

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 import Sidebar from "../components/sidebar/sidebar";
 import Main from "../components/main/main";
-import SearchParams from "../SearchParams";
 
 
 const App = () => {
