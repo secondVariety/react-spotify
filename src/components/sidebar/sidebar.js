4aeab9869dde31f81c6ca8c7f54bc9bdc606d69b
secondVariety <garydavison21@gmail.com>
2021-10-20 11:31:26 +0100

    WIP

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,13 +1,13 @@
 import Player from "../player/player";
 
 const Sidebar = (props) => {
-    return (    
-        <aside>
-            <div id="aside-content">
-                <Player msg="Gary Davison's Covering Letter" />
-            </div>
-        </aside>
-    );
-}
+  return (
+    <aside>
+      <div id="aside-content">
+        <Player msg="Lorem ipsum" />
+      </div>
+    </aside>
+  );
+};
 
 export default Sidebar;
